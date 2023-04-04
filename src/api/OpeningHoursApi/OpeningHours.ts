c3a2485e48b83d9228e43337987a9c19914b8bb5
Gualberto Vannini <gualberto.vannini@gmail.com>
2023-04-04 16:25:10 +0200

    Update OpeningHoursApi response type

--- src/api/OpeningHoursApi/OpeningHours.ts
+++ src/api/OpeningHoursApi/OpeningHours.ts
@@ -1,4 +1,6 @@
+import {AxiosResponse} from 'axios';
 import {gistBucketApiAxios} from '../Apis';
+import {ScheduleState} from './OpeningHoursTypes';
 //there are 2 sample data apis
 //you can switch ROUTE_OPENING_HOURS with ROUTE_OPENING_HOURS_SECOND and have a different dataset
 // import {ROUTE_OPENING_HOURS, ROUTE_OPENING_HOURS_SECOND} from './routes';
@@ -6,7 +8,7 @@ import {gistBucketApiAxios} from '../Apis';
 import {ROUTE_OPENING_HOURS} from './routes';
 
 class OpeningHoursApi {
-  getOpeningHours() {
+  getOpeningHours(): Promise<AxiosResponse<ScheduleState>> {
     return gistBucketApiAxios.get(ROUTE_OPENING_HOURS);
   }
 }

eef3c8ceb8276437c58333f4accd5aa54fd09fa5
GualbertoVannini <gualberto.vannini@gmail.com>
2023-04-14 11:44:58 +0200

    refactor: useDispatch replace useAction

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,10 @@
-import React, {useCallback, useEffect} from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView} from 'react-native';
 import TimeTable from '../../components/Card/TimeTable';
 import loadingSelectors from '../../redux/loading/loadingSelectors';
 import LoadingSpinner from '../../components/LoadingSpinner/';
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {openHoursActions} from '../../redux/openingHours/openHours';
-import useActions from '../../hooks/useActions';
 import styled from 'styled-components/native';
 
 const StyledSafeAreaView = styled(SafeAreaView)`
@@ -15,19 +14,17 @@ const StyledSafeAreaView = styled(SafeAreaView)`
 `;
 
 const Home = () => {
-  const [loadOpeningHours] = useActions([openHoursActions.loadOpeningHours]);
+  const dispatch = useDispatch();
+
+  dispatch([openHoursActions.loadOpeningHours]);
 
   const {isLoading: openHoursLoading} = useSelector(
     loadingSelectors.openHoursRequestState,
   );
 
-  const loadOpenHoursData = useCallback(() => {
-    loadOpeningHours();
-  }, [loadOpeningHours]);
-
   useEffect(() => {
-    loadOpenHoursData();
-  }, [loadOpenHoursData]);
+    dispatch([openHoursActions.loadOpeningHours]);
+  }, [dispatch]);
 
   if (openHoursLoading) {
     return <LoadingSpinner visible={openHoursLoading} />;
